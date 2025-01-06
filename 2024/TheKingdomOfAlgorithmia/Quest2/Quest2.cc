a4773087146cb7b4b784aaff7273b4a9c89551b6
EngineerMinded <wamj283@gmail.com>
2025-01-05 22:53:21 -0500

    Completed Quest 2

--- 2024/TheKingdomOfAlgorithmia/Quest2/Quest2.cc
+++ 2024/TheKingdomOfAlgorithmia/Quest2/Quest2.cc
@@ -56,61 +56,103 @@ int countWordInSentence(string word, string sentence) {
  * determined by a true boolean, count them and you get to the       *
  * answer for part 2.                                                *
  *********************************************************************/
-int countNumberOfRunes (vector<string> words, string Sentence, int wordSize, bool isPart3) {
-    int count = 0;
-    bool validRunes[Sentence.size()];
-    for (int o = 0; o < Sentence.size(); o++) {
-        validRunes[o] = false;
-    }
-    for (int r = 0; r < words.size(); r++) {
-        for (int x = 0; x < Sentence.size() - words[r].size() + 1; x++) {
-            int w = 0;
-            bool wholeWord = true;
-            for(char letter : words[r]) {
-                if (letter != Sentence[x + w]) {
-                    wholeWord = false;
-                }
-                w++;
-            }
-
-            if (!wholeWord) {
-               int v = w - 1;
-               wholeWord = true;
-               for(char letter : words[r]) {
-                    if (letter != Sentence[x + v]) {
-                        wholeWord = false;
-                    }
-                    v--;
-                }
-
-            }
-            if (wholeWord) {
-                for (int i = x; i < x + w; i++ ) {
-                    validRunes[i] = true;
-                } 
-            }
-        }
-    }
-    for (int o = 0; o < Sentence.size(); o++) {
-        if (validRunes[o]) {
-            count ++;
-            //cout << Sentence[o];
-        }
-        else {
-            //cout << '_';
-        }
-    }
-    //cout << endl;
-    return count;
-
+int revisedNumber (int number, int sentenceLimit) {
+    return (number >= sentenceLimit) ? number - (sentenceLimit): number;
 }
 
+void _countNumberOfRunes (vector<vector<bool>>& validRunes, vector<string> words, vector<string> Puzzle, int wordSize, bool isPart3, bool isSecondStepOfPart3) {
+    int row = -1;
+    for (string Sentence : Puzzle) {
+        row++;
+        for (int r = 0; r < words.size(); r++) {
+            int wordSize = (isPart3 && !isSecondStepOfPart3) ? Sentence.size(): Sentence.size() - words[r].size() + 1;
+            for (int x = 0; x < wordSize; x++) {
+                int w = 0;
+                bool wholeWord = true;
+                for(char letter : words[r]) {
+                    if (letter != Sentence[revisedNumber(x + w, Sentence.size())]) {
+                        wholeWord = false;
+                    }
+                    w++;
+                }
 
+                if (!wholeWord) {
+                   int v = w - 1;
+                   wholeWord = true;
+                   for(char letter : words[r]) {
+                        if (letter != Sentence[revisedNumber(x + v, Sentence.size())]) {
+                            wholeWord = false;
+                        }
+                        v--;
+                    }
 
+                }
+                if (wholeWord) {
+                    for (int i = x; i < x + w; i++ ) {
+                        if (isSecondStepOfPart3) {
+                            validRunes[row][revisedNumber(i, Sentence.size())]= true;
+                        }
+                        else {
+                            validRunes[row][revisedNumber(i, Sentence.size())] = true;
+                        }
+                    } 
+                }
+            }
+        }
+    }
+}
+
+int getCountForAnswers(vector<vector<bool>> nodeTable) {
+    int count = 0;
+    for (vector<bool> line : nodeTable) {
+        for (bool thisOne : line) {
+            if (thisOne == 1) count++;
+        }
+    }
+    return count;
+}
+
+int countNumberOfRunes (vector<string> words, vector<string> Puzzle, int wordSize, bool isPart3 ) {
+    vector<vector<bool>> nodeTable, nodeTable2;
+    for (int q = 0; q < Puzzle.size(); q++) {
+        vector<bool> newLine;
+        for (int r = 0; r < Puzzle[q].size(); r++) {
+            newLine.push_back(false);
+        }
+        nodeTable.push_back(newLine);
+    
+    }
+
+    // revise nodeTable by reference
+    _countNumberOfRunes(nodeTable,words,Puzzle,wordSize,isPart3, false);
+    
+    
+    if (isPart3) {
+      
+        vector<string>Puzzle2;
+        for (int q = 0; q < Puzzle[0].length(); q++) {
+            string newLine;
+            vector<bool> newBool;
+            for (int r = 0; r < Puzzle.size(); r++) {
+                newLine = newLine + Puzzle[r][q];
+                newBool.push_back(nodeTable[r][q]);
+                
+            }
+            Puzzle2.push_back(newLine);
+            nodeTable2.push_back(newBool);
+        }
+        _countNumberOfRunes(nodeTable2,words,Puzzle2,wordSize,isPart3, true);
+        
+    }
+    if (isPart3) {
+        return getCountForAnswers(nodeTable2);
+    }
+    else {
+        return getCountForAnswers(nodeTable);
+    }
+}
 
 void part1 () {
-    //string words[] =  {"THE","OWE","MES","ROD","HER"};
-    //string sentence = "AWAKEN THE POWER ADORNED WITH THE FLAMES BRIGHT IRE";
     string words[] = {"LOR","LL","SI","OR","IA","FF","NO"};
     string sentence = "LOREM IPSUM DOLOR SIT AMET, CONSECTETUR ADIPISCING ELIT, SED DO EIUSMOD TEMPOR INCIDIDUNT UT LABORE ET DOLORE MAGNA ALIQUA. UT ENIM AD MINIM VENIAM, QUIS NOSTRUD EXERCITATION ULLAMCO LABORIS NISI UT ALIQUIP EX EA COMMODO CONSEQUAT. DUIS AUTE IRURE DOLOR IN REPREHENDERIT IN VOLUPTATE VELIT ESSE CILLUM DOLORE EU FUGIAT NULLA PARIATUR. EXCEPTEUR SINT OCCAECAT CUPIDATAT NON PROIDENT, SUNT IN CULPA QUI OFFICIA DESERUNT MOLLIT ANIM ID EST LABORUM.";
     int partOne = 0;
@@ -122,27 +164,20 @@ void part1 () {
 
 void part2 () {
     int partTwo = 0;
-    //string words[] = {"THE","OWE","MES","ROD","HER","QAQ"};
-    //string sentences[] = {"AWAKEN THE POWE ADORNED WITH THE FLAMES BRIGHT IRE","THE FLAME SHIELDED THE HEART OF THE KINGS","POWE PO WER P OWE R","THERE IS THE END","QAQAQ"};
     vector<string> words = readWordsFromFile("Q2P2Words.txt");
     vector<string> sentences = readWordsFromFile("Q2P2Sentence.txt");
-    for (string sentence: sentences) {
-            int size = sizeof(words) / sizeof(words[0]);
-        partTwo += countNumberOfRunes(words, sentence, size, false);
-    }
-    cout << "Part Two :" << partTwo << endl;
+    int size = sizeof(words) / sizeof(words[0]);
+    partTwo = countNumberOfRunes(words,sentences,size,false);
+    cout << "Part Two: " << partTwo << endl;
 }
 
-/// PART 3 : COMING SOON
-
 void part3() {
-    int part3 = 0;
-    vector<string>words = readWordsFromFile("part3WordsExample.txt");
-    vector<string>puzzle = readWordsFromFile("part3PuzzleExample.txt");
-    cout << puzzle[0][1] << " PPPP " << puzzle[1][2] << endl;
-
-
-
+    int partThree = 0;
+    vector<string>words = readWordsFromFile("part3Words.txt");
+    vector<string>puzzle = readWordsFromFile("part3Puzzle.txt");
+    int size = sizeof(words) / sizeof(words[0]);
+    partThree = countNumberOfRunes(words,puzzle,size,true);
+    cout << "Part Three: " << partThree<< endl;
 }
 
 int main() {
