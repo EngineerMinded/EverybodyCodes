0161ba1f830b7a033d9f99294d4e89f9d8627429
Wayne Mack <wamj283@gmail.com>
2026-01-31 19:55:31 -0500

    Part 1 and 2 solved

--- 2025/Quest12/Quest.cc
+++ 2025/Quest12/Quest.cc
@@ -31,11 +31,72 @@ vector<vector<int>> readIntGrid(const string &path) {
 	return grid;
 }
 
+int findHighestGreedyApproach(const GRID &grid) {
+	IgnitionList il = IgnitionList();
+	int highest1 = 0;
+	for (int i = 0; i < grid.size(); i++) {
+		for (int j = 0; j < grid[i].size(); j++) {
+			IgnitionList blank = IgnitionList();
+			int ignited = il.ingnitionPathAt(const_cast<GRID&>(grid), i, j, blank);
+			if (ignited > highest1) {
+				highest1 = ignited;
+			}
+		}
+	}
+	int highest2 = 0; /*
+	for (int i = 0; i < grid.size(); i++) {
+		for (int j = 0; j < grid[i].size(); j++) {
+			if (!il.exists(i, j)) {
+				int ignited = il.ingnitionPathAt(const_cast<GRID&>(grid), i, j, il);
+				if (ignited > highest2 && ignited < highest1) {
+					highest2 = ignited;
+				}
+			}
+		}
+	} */
+	int highest3 = 0; /*
+	for (int i = 0; i < grid.size(); i++) {
+		for (int j = 0; j < grid[i].size(); j++) {
+			if (!il.exists(i, j)) {
+				int ignited = il.ingnitionPathAt(const_cast<GRID&>(grid), i, j, il);
+				if (ignited > highest3 && ignited < highest2) {
+					highest3 = ignited;
+				}
+			}
+		}
+	}
+		*/
+	cout << "Top three ignited counts: " << highest1 << ", " << highest2 << ", " << highest3 << endl;
+	return (highest1 + highest2 + highest3);
+}
+
 int main() {
+	// PART ONE
 	GRID example1 = readIntGrid("example1.txt");
 	IgnitionList ex1 = IgnitionList();
 	cout << "Example 1 Ignited Cells: " << ex1.routeIgnitionPath(example1) << endl;	
 
+	GRID part1 = readIntGrid("part1.txt");
+	IgnitionList p1 = IgnitionList();
+	cout << "Part 1 Ignited Cells: " << p1.routeIgnitionPath(part1) << endl;
+
+	// PART TWO
+	GRID example2 = readIntGrid("example2.txt");
+	IgnitionList ex2 = IgnitionList();
+	cout << "Example 2 Ignited Cells: " << ex2.routeIgnitionPath(example2, true) << endl;
+
+	GRID part2 = readIntGrid("part2.txt");
+	IgnitionList p2 = IgnitionList();		
+	cout << "Part 2 Ignited Cells: " << p2.routeIgnitionPath(part2, true) << endl;
+
+	// PART THREE : GREEDY APPROACH
+	GRID example3a = readIntGrid("example3a.txt");
+	cout << "Example 3a Highest Greedy Ignited Cells: " << findHighestGreedyApproach(example3a) << endl;
+
+	GRID example3b = readIntGrid("example3b.txt");
+	cout << "Example 3b Highest Greedy Ignited Cells: " << findHighestGreedyApproach(example3b) << endl;
+
+	return 0;
 }
 
 
--- 2025/Quest12/ignitionlist.hh
+++ 2025/Quest12/ignitionlist.hh
@@ -6,29 +6,35 @@ using namespace std;
 #ifndef IGNITIONLIST_HH
 #define IGNITIONLIST_HH
 
+
 class IgnitionList {
 public:
     int x, y;
     IgnitionList* nextItem;
 
     IgnitionList() {
-        x = 0;
-        y = 0;
+        x = 0; y = 0;
         nextItem = nullptr;
     }
 
     IgnitionList(int xCoord, int yCoord) {
-        x = xCoord;
-        y = yCoord;
+        x = xCoord; y = yCoord;
         nextItem = nullptr;
+    }
+
+        bool exists(int xCoord, int yCoord) {
+        if (x == xCoord && y == yCoord) {
+            return true;
+        } else if (nextItem != nullptr) {
+            return nextItem->exists(xCoord, yCoord);
+        } else {
+            return false;
+        }
     }
 private:
     int countItems() {
-        if (nextItem == nullptr) {
-            return 0;
-        } else {
-            return 1 + nextItem->countItems();
-        }
+        if (nextItem == nullptr) { return 0;} 
+        return 1 + nextItem->countItems();
     }               
 
     void addItem(int xCoord, int yCoord) {
@@ -39,19 +45,17 @@ private:
         }           
     }
 
-    bool exists(int xCoord, int yCoord) {
-        if (x == xCoord && y == yCoord) {
-            return true;
-        } else if (nextItem != nullptr) {
-            return nextItem->exists(xCoord, yCoord);
-        } else {
-            return false;
-        }
-    }   
+    bool complimentaryGridIsTheSame(vector<vector<int>>& grid, int x1, int y1) {
+        int x2 = grid.size() -1 - x1;
+        int y2 = grid[0].size() -1 - y1;
+        return (grid[x1][y1] == grid[x2][y2]);
 
-    void routeIgnitionPath(vector<vector<int>>& grid, int x, int y) {
-        cout << "Igniting cell: (" << x << ", " << y << ") with value " << grid[x][y] << endl;
+    }
+    void routeIgnitionPath(vector<vector<int>>& grid, int x, int y, IgnitionList& il) {
+        //cout << "Igniting cell: (" << x << ", " << y << ") with value " << grid[x][y] << endl;
+
         addItem(x,y);
+    
         // Check all 4 directions
         const vector<pair<int, int>> directions = {{1,0}, {-1,0}, {0,1}, {0,-1}};
         for (const auto& dir : directions) {
@@ -60,19 +64,28 @@ private:
             // Check bounds
             if (newX >= 0 && newX < grid.size() && newY >= 0 && newY < grid[0].size()) {
                 // Check if the cell is ignitable and not already in the list
-                if ((grid[newX][newY] ==  grid[x][y] || grid[newX][newY] < grid[x][y] )&& !exists(newX, newY)) {
-                    routeIgnitionPath(grid, newX, newY);
+                if ((grid[newX][newY] ==  grid[x][y] || grid[newX][newY] < grid[x][y] )&& !exists(newX, newY) && il.exists(newX, newY) == false) {
+                    routeIgnitionPath(grid, newX, newY, il);
                 }
             }
         }
     }
+
 public:
-    int routeIgnitionPath(std::vector<std::vector<int>>& grid) {
-       routeIgnitionPath(grid, 0, 0);
+    int routeIgnitionPath(std::vector<std::vector<int>>& grid, bool complimentary = false) {
+       IgnitionList blank = IgnitionList();
+       routeIgnitionPath(grid, 0, 0, blank);
+       if (complimentary) {
+           routeIgnitionPath(grid, grid.size() - 1, grid[0].size() - 1, blank);
+       }
        return countItems();
     }
     
-    
+    int ingnitionPathAt(std::vector<std::vector<int>>& grid, int x, int y, IgnitionList il) {
+       routeIgnitionPath(grid, x, y, il);
+       return countItems();
+    }
+
 };
 
 #endif // IGNITIONLIST_HH
