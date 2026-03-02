9b76f42d6eb3f844e6e0c496907943cbf996e2e2
Wayne Mack <wamj283@gmail.com>
2026-03-01 21:30:47 -0500

    Part 3 not correct yet

--- 2025/Quest16/Quest.cc
+++ 2025/Quest16/Quest.cc
@@ -10,7 +10,8 @@
 #define NUM_LIST std::vector<int>
 
 /// PART 1 DONE 
-// PART 2 PRACTICAL IS WRONG`
+/// PART 2 DONE
+/// PART 3 TALLYING NUMBERS INCORRECTLY
 
 NUM_LIST readCommaSeparated(const std::string& filename) {
     std::vector<int> nums;
@@ -20,7 +21,6 @@ NUM_LIST readCommaSeparated(const std::string& filename) {
     while (std::getline(file, line)) {
         std::stringstream ss(line);
         std::string token;
-
         while (std::getline(ss, token, ',')) {
             if (!token.empty()) {
                 nums.push_back(std::stoi(token));
@@ -32,8 +32,7 @@ NUM_LIST readCommaSeparated(const std::string& filename) {
 
 NUM_LIST generateWall(NUM_LIST nums, int numberOfPointsInList) {
     NUM_LIST newList;
-    for (int i = 0; i < numberOfPointsInList; i++)
-    {
+    for (int i = 0; i < numberOfPointsInList; i++){
         newList.push_back(0);
     }
     for (int nextNum : nums) {
@@ -59,24 +58,20 @@ bool numberCanBeFactoredOut(NUM_LIST numberList, int numberToTest) {
 
 NUM_LIST generateSpell(NUM_LIST nums, int numberOfPointsInList) {
     NUM_LIST points;
-    for (int i = 1; i < nums.size(); i++)
-    {
+    for (int i = 1; i < nums.size(); i++){
         if (numberCanBeFactoredOut(nums, i)) {
-           
             for (int j = 1; j < nums.size()  + 1; j ++) {
                 if (j % i == 0) {
                     nums[j - 1] -= 1;
                 }
             }
-            std::cout << "Number " << i << " can be factored out.";
-      
-            std::cout << std::endl;
             points.push_back(i);
         }
     }
     return points;
 }
 
+// USE AN INT FOR PART 1 BECAUSE THE NUMBERS ARE SMALLER
 int partOne(NUM_LIST nums, int numberOfPointsInList) {
     NUM_LIST points = generateWall(nums, numberOfPointsInList);
     // INSTRUCTIONS MOVE TO ABOVE FUCTION FOR ERROR CHECKING
@@ -85,18 +80,39 @@ int partOne(NUM_LIST nums, int numberOfPointsInList) {
     return returnThis;
 }
 
-int partTwo(NUM_LIST nums) {
+// USE A LONG LONG FOR PART 2 BECAUSE THE NUMBERS GET REALLY BIG
+long long  partTwo(NUM_LIST nums) {
     NUM_LIST points = generateSpell(nums, nums.size());
-
-    NUM_LIST errorCheck = generateWall(points, nums.size());
-    for (int n = 0; n < errorCheck.size(); n++) {
-        std::cout << errorCheck[n] << "-" << nums[n] << " "; 
-        
-    }
-
-    int returnThis = 1;
+    long long returnThis = 1;
     for (int n : points) {returnThis *= n;}
     return returnThis;
+}
+
+long long gettotalNumberOfBlocksInWall(NUM_LIST nums) {
+    long long total = 0;
+    for (int n : nums) {total += n;}
+    return total;
+}
+
+long long getRemainingWall(NUM_LIST nums, long long totalBlocks) {
+    long long totalWallSize = 0;
+    for (int i = 0; i < nums.size(); i++) {
+        if (totalBlocks < nums[i]) {
+            return totalWallSize;
+        }
+        totalBlocks -= nums[i];
+        totalWallSize += 1;
+    }
+    return totalWallSize;
+}
+
+long long part3(NUM_LIST nums, unsigned long long totalBlocks) {
+    long long totalBlocksInWall = gettotalNumberOfBlocksInWall(nums);
+    std::cout << "Total blocks in wall: " << totalBlocksInWall << std::endl;
+    long long totalWallSize = (totalBlocks / totalBlocksInWall) * nums.size();
+    std::cout << "Total wall size: " << totalWallSize << std::endl;
+    long long remainingBlocks = totalBlocks % totalBlocksInWall;
+    return totalWallSize + getRemainingWall(nums, remainingBlocks);
 }
 
 int main() {
@@ -110,6 +126,14 @@ int main() {
     std::cout << "Example 2: " << partTwo(example2) << std::endl;  
     NUM_LIST part2 = readCommaSeparated("part2.txt");
     std::cout << "Part 2: " << partTwo(part2) << std::endl; 
-    
+    // PART 3
+    NUM_LIST example3 = readCommaSeparated("example2.txt");
+    // ALL SAMPLE CASES
+    std::cout << "Example 3a: " << part3(example3, 1) << std::endl;
+    std::cout << "Example 3b: " << part3(example3, 10) << std::endl;
+    std::cout << "Example 3c: " << part3(example3, 100) << std::endl;
+    std::cout << "Example 3d: " << part3(example3, 1000) << std::endl;
+    std::cout << "Example 3e: " << part3(example3, 10000) << std::endl;
+
     return 0;
 }
